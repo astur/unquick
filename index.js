b7eec3f6224fea33a1fe7c6ffebffe824452e14e
astur <astur@yandex.ru>
2018-07-29 23:49:00 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = () => {};
+module.exports = (fn, delay) => async (...args) => {
+    const [[error, result]] = await Promise.all([
+        fn(...args).then(r => [null, r], e => [e]),
+        new Promise(resolve => setTimeout(resolve, delay)),
+    ]);
+    if(error) throw error;
+    return result;
+};
