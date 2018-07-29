4a4915c914ce987a8eac8601b0a4945bcf97cf19
astur <astur@yandex.ru>
2018-07-29 23:48:52 +0300

    first tests

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
 const test = require('ava');
 const m = require('.');
+const delay = require('delay');
 
-test('unquick', t => {
-    t.true(true);
-    t.is(m, m);
+test('quick', async t => {
+    const start = Date.now();
+    const result = await m(delay, 200)(100, 'OK');
+    t.is(result, 'OK');
+    t.true(Date.now() - start > 200);
 });
 
-test.todo('quick');
-test.todo('slow');
+test('slow', async t => {
+    const start = Date.now();
+    const result = await m(delay, 100)(200, 'OK');
+    t.is(result, 'OK');
+    t.true(Date.now() - start > 200);
+});
+
 test.todo('sync');
 test.todo('reject');
 test.todo('throw');
