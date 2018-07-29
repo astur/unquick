da90f2fd8546fb9e5867489206ee92c61688f42a
astur <astur@yandex.ru>
2018-07-30 00:24:58 +0300

    sync functions support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = (fn, delay) => async (...args) => {
+    const fna = async () => fn(...args); // eslint-disable-line
     const [[error, result]] = await Promise.all([
-        fn(...args).then(r => [null, r], e => [e]),
+        fna(...args).then(r => [null, r], e => [e]),
         new Promise(resolve => setTimeout(resolve, delay)),
     ]);
     if(error) throw error;
