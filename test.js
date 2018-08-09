4d02152ce72f9124eeda297febf472f7fbb1fe33
astur <astur@yandex.ru>
2018-08-09 03:19:29 +0300

    tests fixed for quick machines

--- test.js
+++ test.js
@@ -6,35 +6,35 @@ test('quick', async t => {
     const start = Date.now();
     const result = await m(delay, 200)(100, 'OK');
     t.is(result, 'OK');
-    t.true(Date.now() - start > 200);
+    t.true(Date.now() - start >= 200);
 });
 
 test('slow', async t => {
     const start = Date.now();
     const result = await m(delay, 100)(200, 'OK');
     t.is(result, 'OK');
-    t.true(Date.now() - start > 200);
+    t.true(Date.now() - start >= 200);
 });
 
 test('reject quick', async t => {
     const start = Date.now();
     const result = await m(delay.reject, 200)(100, 'OK').catch(e => `[${e}]`);
     t.is(result, '[OK]');
-    t.true(Date.now() - start > 200);
+    t.true(Date.now() - start >= 200);
 });
 
 test('reject slow', async t => {
     const start = Date.now();
     const result = await m(delay.reject, 100)(200, 'OK').catch(e => `[${e}]`);
     t.is(result, '[OK]');
-    t.true(Date.now() - start > 200);
+    t.true(Date.now() - start >= 200);
 });
 
 test('sync', async t => {
     const start = Date.now();
     const result = await m(v => v, 100)('OK');
     t.is(result, 'OK');
-    t.true(Date.now() - start > 100);
+    t.true(Date.now() - start >= 100);
 });
 
 test('throw', async t => {
@@ -43,5 +43,5 @@ test('throw', async t => {
         throw v;
     }, 100)('OK').catch(e => `[${e}]`);
     t.is(result, '[OK]');
-    t.true(Date.now() - start > 100);
+    t.true(Date.now() - start >= 100);
 });
